97c59cddab27a3a3baae34443cc716ab1c8e4d4d
Hariharnautiyal <101192066+Hariharnautiyal@users.noreply.github.com>
2023-01-28 19:25:54 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { LaunchProgramsComponent } from './launch-programs/launch-programs.component';
-import { LaunchDetailsCardComponent } from './launch-details-card/launch-details-card.component';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LaunchProgramsComponent,
-    LaunchDetailsCardComponent
+
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
